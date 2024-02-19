f840e7030b0ce2cf5156bd9060940ef2c7c891e5
Jack Zhao <jack.chaoy@gmail.com>
2024-02-19 00:36:38 -0500

    Major rewrite with requests
    I split the original script into 3:
    * reschedule.py: the new main applicaiton
    * settings.py: configurations
    * legacy_rescheduler.py: the tested part of the original script for selecting the earliest date and confirm reschedule
    
    Ideally we should also rewrite the legacy rescheduler, but I need an account that I can actually reschedule for testing
    Hope this one isn't as poorly engineered as the previous one XD

---
+++ legacy_rescheduler.py
@@ -0,0 +1,85 @@
+from time import sleep
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+from settings import TEST_MODE
+
+
+# This is frankly very, very bad and should be rewritten with requests
+# when I get a test account
+def reschedule(driver):
+    date_selection_box = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located(
+            (
+                By.XPATH,
+                "/html/body/div[4]/main/div[4]/div/div/form/fieldset/ol/fieldset/div/div[2]/div[3]/label[1]",
+            )
+        )
+    )
+    date_selection_box.click()
+
+    # Move to next month
+    def next_month():
+        driver.find_element(By.XPATH, "/html/body/div[5]/div[2]/div/a").click()
+
+    # Check if avalible in current month
+    def cur_month_ava():
+        month = driver.find_element(By.XPATH, "/html/body/div[5]/div[1]/table/tbody")
+        dates = month.find_elements(By.TAG_NAME, "td")
+        for date in dates:
+            if date.get_attribute("class") == " undefined":
+                ava_date_btn = date.find_element(By.TAG_NAME, "a")
+                return True
+        return False
+
+    # Check the nearest slot is avalible in # months (0 for this month, 1 for next month...) and move to the month
+    def nearest_ava():
+        ava_in = 0
+        cur = cur_month_ava()
+        while not cur:
+            next_month()
+            cur = cur_month_ava()
+            ava_in += 1
+        return ava_in
+
+    avalible_in_months = nearest_ava()
+
+    # Reschedule if the avalible_in_months is less than or equal to wait month
+    print("Trying to pick time and reschedule...")
+    month = driver.find_element(By.XPATH, "/html/body/div[5]/div[1]/table/tbody")
+    dates = month.find_elements(By.TAG_NAME, "td")
+    ava_date_btn = None
+    for date in dates:
+        if date.get_attribute("class") == " undefined":
+            ava_date_btn = date.find_element(By.TAG_NAME, "a")
+            break
+    ava_date_btn.click()
+
+    # Select time of the date:
+    sleep(2)
+    appointment_time = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "appointments_consulate_appointment_time"))
+    )
+    appointment_time.click()
+    appointment_time_options = appointment_time.find_elements(By.TAG_NAME, "option")
+    appointment_time_options[len(appointment_time_options) - 1].click()
+    # select_elem = driver.find_element(By.ID, "appointments_consulate_appointment_time")
+    # select_elem.click()
+    # options = select_elem.find_elements(By.TAG_NAME, "option")
+    # options[len(options) - 1].click()
+
+    # Click "Reschedule"
+    driver.find_element(
+        By.XPATH,
+        "/html/body/div[4]/main/div[4]/div/div/form/div[2]/fieldset/ol/li/input",
+    ).click()
+    try:
+        confirm = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[6]/div/div/a[2]"))
+        )
+    finally:
+        driver.implicitly_wait(0.05)
+        if not TEST_MODE:
+            confirm.click()
---
+++ settings.py
@@ -0,0 +1,29 @@
+# Account Info
+USER_EMAIL = "name@gmail.com"
+USER_PASSWORD = "yourpassword"
+
+# Say you want an appointment no later than Mar 14, 2024
+# Please strictly follow the YYYY-MM-DD format
+LATEST_ACCEPTABLE_DATE = "2024-03-14"
+
+# See the automation in action
+SHOW_GUI = True
+
+# If you just want to see the program run WITHOUT clicking the confirm reschedule button
+# For testing, also set a date really far away so the app actually tries to reschedule
+TEST_MODE = True
+
+# Don't change the following unless you know what you are doing
+DETACH = True
+NEW_SESSION_AFTER_FAILURES = 20
+TIMEOUT = 10
+FAIL_RETRY_DELAY = 10
+BACKOFF_RATE = 2
+REQUEST_DATE_DELAY = 10
+LOGIN_URL = "https://ais.usvisa-info.com/en-ca/niv/users/sign_in"
+APPOINTMENT_PAGE_URL = "https://ais.usvisa-info.com/en-ca/niv/schedule/{id}/appointment"
+AVAILABLE_DATE_REQUEST_SUFFIX = "/days/94.json?appointments[expedite]=false"
+REQUEST_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest",
+}
